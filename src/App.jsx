a23dc65b334f3faadc635d028615d3f949451003
Empyrean Jade <joshuaije3@gmail.com>
2025-03-26 01:04:09 +0100

    cleaned and recached the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import QRcode from "qrcode";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("");
+  const [qrcode, setQrcode] = useState("");
+  const GenerateQrcode = () => {
+    QRcode.toDataURL(
+      url,
+      {
+        width: 800,
+        margin: 2,
+        color: {
+          dark: "#fffffff",
+          light: "#000000ff"
+        }
+      },
+      (err, url) => {
+        if (err) return console.error(err);
 
+        setQrcode(url);
+      }
+    );
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <h1>QR Code Generator</h1>
+      <input
+        type="text"
+        value={url}
+        onChange={(e) => {
+          setUrl(e.target.value);
+        }}
+        placeholder="e.g https://google.com"
+      />
+      <button onClick={GenerateQrcode}>Generate </button>
+      {qrcode && (
+        <>
+          <img src={qrcode} alt={`${qrcode} img`}></img>
+          <a href={qrcode} download="qrcode.jpeg">
+            Download
+          </a>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
